a3146c4a7e792b64d754ec4400941d7501ffcad9
Mert Çağrıberk Erhan <info@cagrierhan.com>
2021-05-26 13:50:17 +0300

    Add project files.

---
+++ JSONFORM/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using JSONFORM.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSONFORM.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		[HttpPost]
+		public IActionResult Submit(TestObject model)
+		{
+			return Json(model);	
+		}
+	}
+
+	public class TestObject
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public bool IsActive { get; set; }
+		public string Gender { get; set; }
+	}
+}
